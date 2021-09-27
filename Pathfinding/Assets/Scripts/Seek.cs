434a6e9b6d9f5bdeaeff8a9b44efa5b30ee7c5ec
J0nathanMB <belmirojonathan2@gmail.com>
2021-09-27 10:11:28 -0300

    Update Seek.cs

--- Pathfinding/Assets/Scripts/Seek.cs
+++ Pathfinding/Assets/Scripts/Seek.cs
@@ -7,9 +7,7 @@ public class Seek : MonoBehaviour
     public GameObject target;
     public float maxVelocity, maxRotation;
 
-    //public Transform[] waypoints;
-
-
+    
     private int current;
 
     // Start is called before the first frame update
@@ -25,16 +23,7 @@ public class Seek : MonoBehaviour
         Vector3 velocity = dir * maxVelocity * Time.deltaTime;
         transform.position += velocity;
 
-        //if (transform.position != waypoints[current].position)
-        //{
-        //    Vector3 dir = (waypoints[current].position - transform.position).normalized;
-        //    Vector3 velocity = dir * maxVelocity * Time.deltaTime;
-        //    transform.position += velocity;
-        //}
-        //else current = (current + 1) % waypoints.Length;
-
-
-
+     
 
         Quaternion rot = Quaternion.LookRotation(dir);
         transform.rotation = Quaternion.Slerp(transform.rotation, rot, maxRotation * Time.deltaTime);
